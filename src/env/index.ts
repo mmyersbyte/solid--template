04deed0ed8f17ec14825af4b6953cba63ae708ea
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2025-12-24 16:42:41 -0300

    Add: Servidor criado e configuração de v.a com tipagem ZOD

---
+++ src/env/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number(), // Se a porta estiver como string, o coerce converte para number()
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error('Variaveis ambiente invalidas', _env.error.format());
+
+  throw new Error('Variaveis de ambiente inválidas');
+}
+
+export const env = _env.data; // O data retorna o objeto validado com as variaveis de ambiente
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import { en } from 'zod/locales';
 import { app } from './app.js';
+import { env } from './env/index.js';
 
 app
   .listen({
-    port: 3333,
     host: '0.0.0.0',
+    port: env.PORT,
   })
   .then(() => {
     console.log(`Server rodando na porta 3333`);
