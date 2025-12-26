73decb86d7c44b962740bfa305efd8e626c7e2da
mmyersbyte <pedrovictorcastrodesousa@gmail.com>
2025-12-26 16:43:25 -0300

    add: hash e validação da senha

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
+import type { FastifyRequest, FastifyReply } from 'fastify';
+import { hash } from 'bcryptjs';
+
 import { prisma } from '@/lib/prisma.js';
 import { z } from 'zod';
-import type { FastifyRequest, FastifyReply } from 'fastify';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,12 +13,25 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: 'Email already in use.',
+    });
+  }
+
   await prisma.user.create({
     data: {
       name: name,
       email: email,
-      password_hash: password,
+      password_hash: password_hash,
     },
   });
+
   return reply.status(201).send();
 }
